3f7b3f8bb9b4c6c8264b76c334d1b19a1e561dcc
enbarSoco <enbar2232@gmail.com>
2022-02-12 16:58:23 +0200

    bug fixed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const logger = require('./config/logger');
 const port = process.env.PORT || 3001;
 app.use(express.json());
@@ -11,12 +10,6 @@ app.use(cors());
 app
     .use('/api/players', playersRouter)
     .use('/api/users', playersRouter);
-const port = process.env.PORT || 3000;
-app.use(express.json());
-const { playersRouter } = require("./routers/playersRouter");
-
-app
-    .use('/api/players', playersRouter);
 
 app
     .use((req, res) => {
