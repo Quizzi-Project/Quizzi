7f3cc781e1192582c8dfca3ab13efd1f44d57f23
Nitsan <nitsan.b5@gmail.com>
2022-02-13 18:10:15 +0200

    modified files

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,68 +1,71 @@
 const Player = require('../models/player');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 exports.playersController = {
     getPlayers(req, res) {
         Player.find({}, { '__v': 0 })
             .then(docs => { res.json(docs) })
-            .catch(err => res.status(400).send({"error":`Error getting Data from DB: ${err}`}));
+            .catch(err => res.status(400).send({ "error": `Error getting Data from DB: ${err}` }));
     },
 
     getPlayer(req, res) {
         Player.find({ id: req.params.id }, { '__v': 0 })
-            .then(data => { 
-                if(!data.length) return res.status(404).json({"message": "Player does not exist"});
-                else { res.status(200).json(data); } })
-            .catch(err => res.status(400).send({"error":`Error getting Data from DB: ${err}`}));
-    },
-    
-    addPlayer(req, res) {
-        let playerData;
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-            playerData = {
-                "name": req.body.name,
-                "username": req.body.username,
-                "email": req.body.email,
-                "password": hash,
-            }
-        
-            const newPlayer = new Player(playerData);
-            const result = newPlayer.save(); 
-            if(result) {
-                res.json({"message":"Player added successfully"});
-            } else {
-                res.status(404).json({"error":"Error registering new player"});
-            }
-        });
+            .then(data => {
+                if (!data.length) return res.status(404).json({ "message": "Player does not exist" });
+                else { res.status(200).json(data); }
+            })
+            .catch(err => res.status(400).send({ "error": `Error getting Data from DB: ${err}` }));
     },
 
-    approvePlayer(req, res) {
-        Player.find({ username: req.body.username })
-            .then(result => { bcrypt.compare(req.body.password, docs[0]['password'], (err, result) => {
-                if(result) {
-                    res.json({"message": "Player logged in successfully"})
-                }
-                else { res.json({"message": "Player info doesn't match" }) }
-                });  
-            })
-            .catch(err => {res.status(400).json({"error":`Error getting data from DB: ${err}`})
-        }); 
+    addPlayer(req, res) {
+        const newPlayer = new Player(req.body);
+        newPlayer.save()
+            .then(result => { res.json({ status: 'ok' }); })
+            .catch(err => { res.status(400).json(err.message); })
     },
+
 
     updatePlayer(req, res) {
-        Player.updateOne({id: req.params.id}, req.body)
-           .then(data => {
-                   if (!data.modifiedCount) return res.status(404).json({"message":"Nothing to update!"});
-                       res.json({"message":"Player Updated"}); })
-            .catch(err => res.status(400).send({"error":`Error getting Data from DB: ${err}`}));
-   },
-
-   deletePlayer(req, res) {
-        Player.deleteOne({id: req.params.id})
+        Player.updateOne({ id: req.params.id }, req.body)
             .then(data => {
-                if(!data.deletedCount) return res.status(404).send({"message":"Player to be deleted not found"});
-                res.json({"message":"Player has been deleted successfully"});
+                if (!data.modifiedCount) return res.status(404).json({ "message": "Nothing to update!" });
+                res.json({ "message": "Player Updated" });
             })
-            .catch(err => res.status(400).send({"error":`Error deleting player from DB: ${err}`}));
-   }
+            .catch(err => res.status(400).send({ "error": `Error getting Data from DB: ${err}` }));
+    },
+
+    deletePlayer(req, res) {
+        Player.deleteOne({ id: req.params.id })
+            .then(data => {
+                if (!data.deletedCount) return res.status(404).send({ "message": "Player to be deleted not found" });
+                res.json({ "message": "Player has been deleted successfully" });
+            })
+            .catch(err => res.status(400).send({ "error": `Error deleting player from DB: ${err}` }));
+    },
+    login(req, res) {
+        const { email, password } = req.body;
+
+        Player.find({ email })
+            .then((data) => {
+                if (data.length === 0) {
+                    return res.status(401).json({ message: "Auth failed" });
+                }
+                const [player] = data;
+                bcrypt.compare(password, player.password, (error, result) => {
+                    if (error) {
+                        return res.status(401).json({ message: "Auth failed" });
+                    }
+
+                    if (result) {
+                        const token = jwt.sign({ _id: data[0]._id }, process.env.JWT_KEY, { expiresIn: "10H" });
+                        return res.status(200).json({
+                            message: 'Auth successful',
+                            token
+                        })
+                    }
+                    res.status(401).json({ message: "Auth failed" });
+                });
+            });
+    }
 }
--- models/player.js
+++ models/player.js
@@ -1,27 +1,34 @@
-const { Schema, model } = require('mongoose'),
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-  playerSchema = new Schema({
-    id: { type: Number, default: 0, unique: true },
-    name: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Enter a valid email address'], unique: true },
-    password: { type: String },
-    registeredAt: { type: Date, default: Date.now },
-    lastLogin: { type: Date, default: Date.now },
-    pointsEarned: { type: Number, default: 0 },
-    gamesWins: { type: Number, default: 0 },
-    gamesLosses: { type: Number, default: 0 }
-  }, { collections: 'players' });
+const playerSchema = new Schema({
+  id: { type: Number, default: 0, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, unique: true, match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Enter a valid email address'], required: true, autoIndex: true, },
+  password: { type: String, required: true },
+  registeredAt: { type: Date, default: Date.now },
+  lastLogin: { type: Date, default: Date.now },
+  pointsEarned: { type: Number, default: 0 },
+  gamesWins: { type: Number, default: 0 },
+  gamesLosses: { type: Number, default: 0 },
+}, { collections: 'players' });
 
 
-playerSchema.
-  pre("save", function (next) {
+playerSchema
+  .pre("save", function (next) {
     Player.find().sort({ "id": -1 })
       .then((data) => {
         this.id = data[0].id + 1;
         next();
       });
   })
+  .pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
 
+
+autoIndex: true;
 const Player = model('Player', playerSchema);
 module.exports = Player;
