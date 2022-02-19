91a0e4660e598787afe39ffa7acc26fe6eb679b1
Nitsan <nitsan.b5@gmail.com>
2022-02-19 20:29:07 +0200

    removed comments

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 const cors = require('cors')
 const { playersRouter } = require("./routers/playersRouter");
-// const { managerRouter } = require("./routers/managerRouter");
 app.use(cors());
 
 app
     .use('/api/players', playersRouter)
     .use('/api/users', playersRouter)
-    // .use('/api/managers', managerRouter);
 
 app
     .use((req, res) => {
@@ -20,3 +18,4 @@ app
     .listen(port, () => {
         console.log(`Listening on port ${port}...`);
     });
+
